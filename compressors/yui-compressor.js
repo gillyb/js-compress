0fe94d649b0e04891de43f85786daa1bcc245969
gillyb <gillyb@gmail.com>
2014-08-31 22:12:17 +0300

    initial commit with yui wrapper

---
+++ compressors/yui-compressor.js
@@ -0,0 +1,31 @@
+
+var compressor = require('yuicompressor');
+var q = require('q');
+
+var YuiCompressor = {
+
+	compressJs: function(js) {
+		var deferred = q.defer();
+		compressor.compress(js, {
+			charset: 'utf8',
+			type: 'js',
+			nomunge: true // don't obfuscate, only minify
+		},
+		function (err, data, extra) {
+			deferred.resolve(data);
+		}
+		return deferred.promise;
+	},
+	compressCss: function(css) {
+		var deferred = q.defer();
+		compressor.compress(css, {
+			charset: 'utf8',
+			type: 'css'
+		},
+		function (err, data, extra) {
+			deferred.resolve(data);
+		}
+		return deferred.promise;
+	}
+
+};

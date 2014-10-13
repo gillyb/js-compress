e28c9d1e3a7393e8a1a7b87c569e2834bd62f8cc
Gilly Barr <gillyb@gmail.com>
2014-10-13 23:20:05 +0300

    fixed some basic file-upload.js bugs

--- scripts/file-upload.js
+++ scripts/file-upload.js
@@ -1,92 +1,96 @@
 
-var holder = document.getElementById('holder'),
+var holder = $('#holder'),
     tests = { // check for compatibility
-      filereader: typeof FileReader != 'undefined',
-      dnd: 'draggable' in document.createElement('span'),
-      formdata: !!window.FormData,
-      progress: "upload" in new XMLHttpRequest
+        filereader: typeof(FileReader) != 'undefined',
+        dnd: 'draggable' in document.createElement('span'),
+        formdata: !!window.FormData,
+        progress: "upload" in new XMLHttpRequest
     },
     support = { // check support for certain html5 elements
-      filereader: document.getElementById('filereader'),
-      formdata: document.getElementById('formdata'),
-      progress: document.getElementById('progress')
+        filereader: $('#filereader'),
+        formdata: $('#formdata'),
+        progress: $('#progress')
     },
     acceptedTypes = { // types we can accept
-      'image/png': true, // TODO: change this to js/css only!
-      'image/jpeg': true,
-      'image/gif': true
+        'image/png': true, // TODO: change this to js/css only!
+        'image/jpeg': true,
+        'image/gif': true
     },
-    progress = document.getElementById('uploadprogress'),
-    fileupload = document.getElementById('upload');
+    progress = $('#uploadprogress'),
+    fileupload = $('#upload');
 
 ['filereader', 'formdata', 'progress'].forEach(function (api) {
-  if (tests[api] === false) {
-    support[api].className = 'fail';
-  } else {
-    // FFS. I could have done el.hidden = true, but IE doesn't support
-    // hidden, so I tried to create a polyfill that would extend the
-    // Element.prototype, but then IE10 doesn't even give me access
-    // to the Element object. Brilliant.
-    support[api].className = 'hidden';
-  }
+    if (tests[api] === false) {
+        support[api].addClass('fail');
+    } else {
+        support[api].addClass('hidden');
+    }
 });
 
 function previewfile(file) {
-  if (tests.filereader === true && acceptedTypes[file.type] === true) {
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var image = new Image();
-      image.src = event.target.result;
-      image.width = 250; // a fake resize
-      holder.appendChild(image);
-    };
+    if (tests.filereader === true && acceptedTypes[file.type] === true) {
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            var image = new Image();
+            image.src = event.target.result;
+            image.width = 250; // a fake resize
+            holder.append(image);
+        };
 
-    reader.readAsDataURL(file);
-  }  else {
-    holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size/1024|0) + 'K' : '');
-    console.log(file);
-  }
+        reader.readAsDataURL(file);
+    } else {
+        holder.html(holder.html() + '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size / 1024 | 0) + 'K' : ''));
+        console.log(file);
+    }
 }
 
 function readfiles(files) {
     var formData = tests.formdata ? new FormData() : null;
     for (var i = 0; i < files.length; i++) {
-      if (tests.formdata) formData.append('file', files[i]);
-      previewfile(files[i]);
+        if (tests.formdata) formData.append('file', files[i]);
+        previewfile(files[i]);
     }
 
     // now post a new XHR request
     if (tests.formdata) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/upload');
-      xhr.onload = function() {
-        progress.value = progress.innerHTML = 100;
-      };
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/upload');
+        xhr.onload = function () {
+            progress.value = progress.innerHTML = 100;
+        };
 
-      if (tests.progress) {
-        xhr.upload.onprogress = function (event) {
-          if (event.lengthComputable) {
-            var complete = (event.loaded / event.total * 100 | 0);
-            progress.value = progress.innerHTML = complete;
-          }
+        if (tests.progress) {
+            xhr.upload.onprogress = function (event) {
+                if (event.lengthComputable) {
+                    var complete = (event.loaded / event.total * 100 | 0);
+                    progress.value = progress.innerHTML = complete;
+                }
+            }
         }
-      }
 
-      xhr.send(formData);
+        xhr.send(formData);
     }
 }
 
 if (tests.dnd) {
-  holder.ondragover = function () { this.className = 'hover'; return false; };
-  holder.ondragend = function () { this.className = ''; return false; };
-  holder.ondrop = function (e) {
-    this.className = '';
-    e.preventDefault();
-    readfiles(e.dataTransfer.files);
-  }
-} else {
-  fileupload.className = 'hidden';
-  fileupload.querySelector('input').onchange = function () {
-    readfiles(this.files);
-  };
+    holder.on('dragover', function () {
+        $(this).addClass('hover');
+        return false;
+    });
+    holder.on('dragend', function () {
+        $(this).removeClass('hover');
+        return false;
+    });
+    holder.on('drop', function (e) {
+        $(this).removeClass('hover');
+        e.preventDefault();
+        e.stopPropagation();
+        readfiles(e.originalEvent.dataTransfer.files);
+    });
+}
+else {
+    fileupload.addClass('hidden');
+    fileupload.find('input').on('change', function () {
+        readfiles(this.files);
+    });
 }

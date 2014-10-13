a0b42f6e6035a0fb89f0721a389eed704aa9c623
Gilly Barr <gillyb@gmail.com>
2014-10-13 22:27:12 +0300

    displaying all minified results

--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,81 @@
 
 $(function() {
 
-	var filesTab = $('.files-tab'),
-		textTab = $('.text-tab'),
-		textTabContainer = $('.text-tab-container'),
-		filesTabContainer = $('.files-tab-container'),
-		compressButton = $('#compress-js-button'),
-		outputDetails = $('.output-details');
+    var filesTab = $('.files-tab'),
+        textTab = $('.text-tab'),
+        textTabContainer = $('.text-tab-container'),
+        filesTabContainer = $('.files-tab-container'),
+        compressButton = $('#compress-js-button'),
+        outputDetails = $('.output-details');
 
-	var compressText = textTab.hasClass('active');
+    var compressText = textTab.hasClass('active');
 
-	$('.files-tab').click(function() {
-		textTabContainer.addClass('hidden');
-		textTab.removClass('active');
-		filesTabContainer.removeClass('hidden');
-		filesTab.addClass('active');
-	});
-	$('.text-tab').click(function() {
-		filesTabContainer.addClass('hidden');
-		filesTab.removeClass('active');
-		textTabContainer.removeClass('hidden');
-		textTab.addClass('active');
-	});
+    $('.files-tab').click(function() {
+        textTabContainer.addClass('hidden');
+        textTab.removClass('active');
+        filesTabContainer.removeClass('hidden');
+        filesTab.addClass('active');
+    });
+    $('.text-tab').click(function() {
+        filesTabContainer.addClass('hidden');
+        filesTab.removeClass('active');
+        textTabContainer.removeClass('hidden');
+        textTab.addClass('active');
+    });
 
-	compressButton.click(function() {
-		if (compressText)
-			compressJsText();
-		else
-			compressJsFiles();
-	});
+    compressButton.click(function() {
+        if (compressText)
+            compressJsText();
+        else
+            compressJsFiles();
+    });
 
-	function compressJsText() {
-		var formData = new FormData();
-		formData.append('jsInput', $('#js-input').val());
-		formData.append('jsCompressor', $('input[name=compressor-type]:checked').val());
+    function compressJsText() {
+        var formData = new FormData();
+        formData.append('jsInput', $('#js-input').val());
+        formData.append('jsCompressor', $('input[name=compressor-type]:checked').val());
 
-		$.ajax({
-			url: '/compress-text',
-			type: 'POST',
-			data: formData,
-			contentType: false,
-			processData: false
-		}).success(function(res) {
-			//res.compressor, res.prev_data_size, res.new_data_size
-			outputDetails.append(createResultTemplate(res[0].compressor, res[0].prev_data_size, res[0].new_data_size));
-			$('#js-input').val(res[0].compressed);œ
-		}).error(function(e) {
-			alert(e);
-		});
-	}
+        $.ajax({
+            url: '/compress-text',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false
+        }).success(function(res) {
+            //res.compressor, res.prev_data_size, res.new_data_size
+            var mostCompressed = 0;
+            var smallest = res[0].prev_data_size;
+            for (var i=0; i<res.length; i++) {
+                outputDetails.append(createResultTemplate(res[0].compressor, res[0].prev_data_size, res[0].new_data_size));
+                if (res[0].new_data_size < smallest) {
+                    smallest = res[0].new_data_size;
+                    mostCompressed = i;
+                }
+            }
 
-	function compressJsFiles() {
+            $('#js-input').val(res[mostCompressed].compressed);
+        }).error(function(e) {
+            alert(e);
+        });
+    }
 
-	}
+    function compressJsFiles() {
 
-	function createResultTemplate(compressor, prev_size, new_size) {
-		function createFieldValue(field, value) {
-			var f = $('<span/>').addClass('label').html(field);
-			var v = $('<span/>').addClass('value').html(value);
-			return $('<div/>').append(f).append(v);
-		}
+    }
 
-		var wrappingDiv = $('<div/>').addClass('output-result');
-		wrappingDiv.append(createFieldValue('Compressor : ', compressor));
-		wrappingDiv.append(createFieldValue('original size : ', prev_size));
-		wrappingDiv.append(createFieldValue('compressed size : ', new_size));
+    function createResultTemplate(compressor, prev_size, new_size) {
+        function createFieldValue(field, value) {
+            var f = $('<span/>').addClass('label').html(field);
+            var v = $('<span/>').addClass('value').html(value);
+            return $('<div/>').append(f).append(v);
+        }
 
-		return wrappingDiv;
-	}
+        var wrappingDiv = $('<div/>').addClass('output-result');
+        wrappingDiv.append($('<div/>').addClass('compressor').html(compressor));
+        wrappingDiv.append(createFieldValue('original size : ', prev_size));
+        wrappingDiv.append(createFieldValue('compressed size : ', new_size));
+
+        return wrappingDiv;
+    }
 
 });
